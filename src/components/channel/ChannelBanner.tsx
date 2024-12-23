0e7ffe65649b32b4b7ca71f0b60acf3e8cab76d4
FirmanKhoiril <firmankhoiril13@gmail.com>
2024-12-23 19:36:52 +0700

    first initializing project structure

---
+++ src/components/channel/ChannelBanner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChannelBanner = () => {
+  return (
+    <div>ChannelBanner</div>
+  )
+}
+
+export default ChannelBanner
---
+++ src/services/api/FetchYoutube.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const Base_url = "https://youtube-v31.p.rapidapi.com/";
+
+const options = {
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_APP_RAPID_API_KEY,
+    "X-RapidAPI-Host": "youtube-v38.p.rapidapi.com",
+  },
+};
+
